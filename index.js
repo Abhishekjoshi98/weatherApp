84cd56cf7db5cce6e8b95433c5a7cfd917531bca
Abhishekjoshi98 <98544171+Abhishekjoshi98@users.noreply.github.com>
2023-01-15 16:21:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function detailsOfCityWheather() {
         tr.appendChild(td6);
         Tbody.appendChild(tr);
         btn12.addEventListener("click", () => {
-          remove(tr);
+          deleteRow(tr);
         });
         Cities[i - 1].classList.add("sidebar-table-border");
         let nodata = document.getElementById("nodata-container");
@@ -44,22 +44,45 @@ function detailsOfCityWheather() {
   }
   i++;
 }
-function remove(e){
+function deleteRow(e){
   e.remove();
 }
 
-function compareSidebarData() {
-  let input1 = document.getElementById("input-data-for-side").value;
-  let tr = document.getElementsByTagName("tr");
-  if (input1 === "London") {
-    tr[1].classList.add("bg");
-  } else if (input1 === "New York") {
-    tr[2].classList.add("bg");
-  } else if (input1 === "Los Angeles") {
-    tr[3].classList.add("bg");
-  } else if (input1 === "Las Vegas") {
-    tr[4].classList.add("bg");
-  } else {
-    return;
-  }
+function getData(){
+  const input1 = document.getElementById("input-data-for-side");
+     const searchedText = input1.value
+    fetch(`https://python3-dot-parul-arena-2.appspot.com/test?cityname=${searchedText}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        let tbody = document.getElementById("TBody");
+        let tr = document.createElement("tr");
+        let td1 = document.createElement("td");
+        td1.innerHTML = searchedText;
+        let td2 = document.createElement("td");
+        td2.innerHTML = data.description;
+        let td3 = document.createElement("td");
+        td3.innerHTML = data.temp_in_celsius;
+        let td4 = document.createElement("td");
+        td4.innerHTML = data.humidity_in_percent;
+        let td5 = document.createElement("td");
+        td5.innerHTML = data.date_and_time;
+        let td6 = document.createElement("td");
+        let btn = document.createElement("button");
+        btn.innerText = "Delete";
+        td6.appendChild(btn);
+        btn.classList.add("jb");
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+        tr.appendChild(td5);
+        tr.appendChild(td6);
+        tbody.appendChild(tr);
+        btn.addEventListener("click", () => {
+          deleteRow(tr);
+        });
+       } )
+       
 }
